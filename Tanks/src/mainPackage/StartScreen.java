fcd3aadc9a72073365c59b00ae31ee0d2e0e3ea6
Nikidos18 <163156301+Nikidos18@users.noreply.github.com>
2025-01-11 22:25:27 +0200

    Add start screen file

---
+++ Tanks/src/mainPackage/StartScreen.java
@@ -0,0 +1,66 @@
+package mainPackage;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class StartScreen extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartScreen frame = new StartScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StartScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Start");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFrame World=new World();
+				World.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(140, 146, 150, 54);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Tanks");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 37));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(73, 33, 279, 100);
+		contentPane.add(lblNewLabel);
+	}
+}
